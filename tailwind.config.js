81307fb12f8033a6c6eb517c06fb8bb148cfbcc5
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2022-02-12 14:39:44 +0600

    WIP
    WORKING IN HEADER

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./output/script.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'guide-orange':"hsl(26, 100%, 55%)",
+        'guide-pale-orange':"hsl(25, 100%, 94%)",
+        'very-vary-blue':"hsl(220, 13%, 13%)",
+        'dark-grayish-blue':"hsl(219, 9%, 45%)",
+        'grayish-blue':"hsl(220, 14%, 75%)",
+        'light-grayish-blue':'hsl(223, 64%, 98%)',
+        'white':"hsl(0, 0%, 100%)",
+        'black':"hsl(0, 0%, 0%)"
+      },
+      spacing:{
+        twenty:"20%",
+        fourty:"40%",
+      }
+    },
   },
   plugins: [],
 }
